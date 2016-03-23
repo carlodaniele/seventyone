ea143cd18dd9e99759374ba433206d6a4ec14d7b
carlodaniele <carlo.daniele@gmail.com>
2016-03-23 21:22:17 +0100

    Update template-author.php

--- template_parts/template-author.php
+++ template_parts/template-author.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * @package WordPress
- * @subpackage Seventy-One-WordPress-Theme
- * @since Seventy One 1.0
+ * @subpackage SeventyOne
+ * @since SeventyOne 1.0
  */
  ?>
 			<aside class="author-box">
 				<?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
-				<h4><?php echo __('About ') . get_the_author_meta( 'display_name' ); ?></h4>
+				<h4><?php echo __('About', 'seventyone' ) . get_the_author_meta( 'display_name' ); ?></h4>
 				<p>
 					<?php the_author_meta( 'description' ); ?><br />
 					<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
-						<?php echo __('View all posts by ') . get_the_author() . ' (' . get_the_author_posts() .')'; ?>
+						<?php echo __('View all posts by', 'seventyone' ) . get_the_author() . ' (' . get_the_author_posts() .')'; ?>
 					</a>
 				</p>
 			</aside>
